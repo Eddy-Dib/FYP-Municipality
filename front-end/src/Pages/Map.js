86e81f4db78fac811996c6befddddcfedcf20819
yvana flayhan <flayhanyvana1@gmail.com>
2026-04-15 15:46:06 +0300

    add map for home page

---
+++ front-end/src/Pages/Map.js
@@ -0,0 +1,11 @@
+import styles from "./Map.module.css";
+function Map(){
+    return(
+        <div className={styles.mapSection}>
+            <iframe>
+
+            </iframe>
+        </div>
+    );
+}
+export default Map;
