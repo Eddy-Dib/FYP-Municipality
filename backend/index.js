7c5556d78d57da3e6cd98419da5a57a355817138
yvana flayhan <flayhanyvana1@gmail.com>
2026-04-23 21:52:23 +0300

    backend of citizen messages

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import complaintRoutes from "./routes/complaintRoutes.js";
 
 const app = express();
 
@@ -10,6 +11,10 @@ app.use(json());
 // Sends all requests starting with /auth to authRoutes
 app.use("/auth", authRoutes);
 
+// Routes for handling complaints and citizen messages (stored in COMPLAINT table)
+app.use("/api/complaints", complaintRoutes);
+
 // Start server. Backup port: 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
